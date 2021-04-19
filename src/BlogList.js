eb557b63f87538a753da857b6091b719d19751d9
Umut Akin <umutakin@fastmail.com>
2021-04-19 11:02:30 +0300

    react tutorial 13 - functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,16 @@
 //const BlogList = (props) => {
-const BlogList = ({blogs, title}) => {
-
+const BlogList = ({ blogs, title, handleDelete }) => {
     // const blogs = props.blogs;
     // const title = props.title;
 
     return (
         <div className="class-list">
-            <h2>{ title }</h2>
+            <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
