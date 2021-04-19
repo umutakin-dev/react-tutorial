50245872d992d83f5a8c8e83b1fb31da9df7eaa9
Umut Akin <umutakin@fastmail.com>
2021-04-19 23:29:43 +0300

    react tutorial 32 - 404 pages

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -12,7 +12,7 @@ const BlogDetails = () => {
         fetch("http://localhost:8000/blogs/" + id, {
             method: "DELETE",
         }).then(() => {
-            history.push('/');
+            history.push("/");
         });
     };
 
