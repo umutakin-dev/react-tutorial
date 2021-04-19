08e6056933887be99e1730f4a5ec8193ea0c568c
Umut Akin <umutakin@fastmail.com>
2021-04-19 22:24:11 +0300

    react tutorial 25 - route parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,15 @@
-//const BlogList = (props) => {
-const BlogList = ({ blogs, title }) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
+import { Link } from "react-router-dom";
 
+const BlogList = ({ blogs, title }) => {
     return (
         <div className="class-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
