3f538345c46cfe5e09b86d86fb8dc2337e5f86de
Umut Akin <umutakin@fastmail.com>
2021-04-19 09:59:46 +0300

    react tutorial 07 - click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={ handleClick }>Click Me</button>
+            <button onClick={ (e) => handleClickAgain('mario', e) }>Click Me Again</button>
         </div>
     );
 }
