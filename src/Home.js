ab50ad9fbb666bda665db13cac59728f3c569fc0
Umut Akin <umutakin@fastmail.com>
2021-04-19 19:08:44 +0300

    react tutorial 21 - the react router

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,6 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    // const [blogs, setBlogs] = useState(null);
-    // const [isPending, setIsPending] = useState(true);
-    // const [error, setError] = useState(null);
-
     const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
 
     return (
