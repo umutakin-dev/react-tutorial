b8e512801fc3a7cc98fb8391f0ad990d3a5a8c62
Umut Akin <umutakin@fastmail.com>
2021-04-19 09:45:26 +0300

    react tutorial 06 - adding styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import "./App.css";
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-
-    const title = 'Welcome to the new blog';
-    const likes = 50;
-    const person = { name: 'umut', age:30 };
-    const link = 'https://www.google.com';
-
     return (
         <div className="App">
+            <Navbar />
             <div className="content">
-                <h1>{ title }</h1>
-                <p>Liked: { likes }</p>
-                {/* <p>{ person }</p> */}
-                <p>{ 10 }</p>
-                <p>{ "hello ninjas" }</p>
-                <p>{ [1, 2, 3, 4, 5] }</p>
-                <p>{ Math.random() * 10 }</p>
-
-                <a href={ link }>Google</a>
+                <Home />
             </div>
         </div>
     );
